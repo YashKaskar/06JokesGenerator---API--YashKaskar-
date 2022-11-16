7e2f339395b20e9eeb0138887dab7eea58c25af2
Yash Kaskar <yash.kaskar99@gmail.com>
2022-11-16 10:52:18 +0530

    async await

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const joke = document.querySelector('#joke');
 const btnjoke = document.querySelector('#jokeBtn');
 
 btnjoke.addEventListener('click', generatingthejoke);
+async function generatingthejoke() { 
+    const jokes = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            accept: 'application/json'
+        }
+    });
+}
